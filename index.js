7a61ead85aefb2a006f8c8474a60a4c9630b09cf
ciobanuradu96 <ciobanu.radu96@gmail.com>
2021-03-17 16:32:31 +0200

    meh

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function getInputValue() {
-    let inputVal = document.getElementById("myInput").value;
-    document.getElementById("myInput").value = "";
+document.getElementById("add").addEventListener("click",()=>{
+    let inputVal=[]
+    inputVal = document.getElementById("myInput").value;
     console.log(inputVal)
-}
+    document.getElementById("myInput").value = "";
+    let div=document.createElement("div");
+    div.setAttribute("class","col-md-12 col-sm-12 col-xs-12")
+    div.setAttribute("align","center")
+    div.innerHTML=inputVal
+    document.getElementById("results").appendChild(div);
+     },false)
+
+     
+   
+// for every click id=i+1 which gets added to 
+//the div id in order to be able to delete it 
