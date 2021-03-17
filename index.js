bd3db8c38a4e0cde065f457b02d43b19e1b96e81
ciobanuradu96 <52462743+ciobanuradu96@users.noreply.github.com>
2021-03-17 19:10:00 +0200

    final

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-document.getElementById("add").addEventListener("click",()=>{
-    let inputVal=[]
-    inputVal = document.getElementById("myInput").value;
-    console.log(inputVal)
-    document.getElementById("myInput").value = "";
-    let div=document.createElement("div");
-    div.setAttribute("class","col-md-12 col-sm-12 col-xs-12")
-    div.setAttribute("align","center")
-    div.innerHTML=inputVal
-    document.getElementById("results").appendChild(div);
-     },false)
+let addButton = document.getElementById("add");
+let inputField = document.getElementById("myInput");
+let resultArea = document.getElementById("results");
 
-     
-   
-// for every click id=i+1 which gets added to 
-//the div id in order to be able to delete it 
+addButton.addEventListener("click", () => {
+    if (inputField.value != "") {
+        inputVal = inputField.value;
+        inputField.value = "";
+        let div = document.createElement("div");
+        div.setAttribute("class", "col-md-12 col-sm-12 col-xs-12")
+        div.setAttribute("align", "center")
+        div.innerHTML = inputVal
+        resultArea.appendChild(div);
+        let removeButton = document.createElement("i");
+        removeButton.setAttribute("class", "fas fa-minus-square  remove-sign");
+        div.appendChild(removeButton);
+        removeButton.addEventListener("click", () => {
+            div.removeChild(removeButton);
+            resultArea.removeChild(div);
+        })
+    }
+}, false)
+
